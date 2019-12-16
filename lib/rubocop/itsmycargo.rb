7bc0e87cba34bc85583cce882a854c0a457c487e
Mikko Kokkonen <mikko.kokkonen@itsmycargo.com>
2019-12-16 16:01:22 +0100

    Initial Commit

---
+++ lib/rubocop/itsmycargo.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'rubocop/itsmycargo/version'
+
+module Rubocop
+  module Itsmycargo
+  end
+end
---
+++ spec/rubocop/itsmycargo_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe Rubocop::Itsmycargo do
+  it 'has a version number' do
+    expect(Rubocop::Itsmycargo::VERSION).not_to be nil
+  end
+end
