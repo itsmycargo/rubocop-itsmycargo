5b37be56c212839146843846285eed5417daff6e
Mikko Kokkonen <mikko.kokkonen@itsmycargo.com>
2020-08-07 09:41:49 +0700

    feat: forbid disabling rubocop

--- lib/rubocop/itsmycargo.rb
+++ lib/rubocop/itsmycargo.rb
@@ -1,8 +1,3 @@
 # frozen_string_literal: true
 
 require "rubocop/itsmycargo/version"
-
-module Rubocop
-  module Itsmycargo
-  end
-end
--- lib/rubocop/itsmycargo/version.rb
+++ lib/rubocop/itsmycargo/version.rb
@@ -2,6 +2,6 @@
 
 module Rubocop
   module Itsmycargo
-    VERSION = "0.2.6"
+    VERSION = "0.2.7"
   end
 end
